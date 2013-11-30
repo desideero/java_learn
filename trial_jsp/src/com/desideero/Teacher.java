f2debcd81727e319a6965a869fdf0d3e92d88f00
desideero <alkeer28@gmail.com>
2013-12-01 01:00:22 +0200

    Teacher.java isimli java sınıf da eklendi. Hayırlı olsun.

---
+++ trial_jsp/src/com/desideero/Teacher.java
@@ -0,0 +1,17 @@
+package com.desideero;
+
+import java.util.ArrayList;
+
+public class Teacher {
+	int civilIDNo;
+	int age;
+	
+	String name;
+	String surname;
+	String alias;
+	
+	ArrayList<String> coursesToGive;
+	
+	
+
+}
